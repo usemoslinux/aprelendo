43f14c28c58338ee8162bb11afba6893de9567a2
usemoslinux <pablo.castagnino@gmail.com>
2018-05-06 19:45:36 +0200

    Fixes #29

--- public/archivedtexts.php
+++ public/archivedtexts.php
@@ -3,12 +3,26 @@
 	require_once('header.php')
 	?>
 
-	 <div class="container mtb">
-	 	<div class="row">
-	 		<div class="col-lg-12">
-				<?php $showarchivedtexts = true; require_once('listtexts.php'); ?>
-			</div>
-	 	</div>
-	 </div>
+  <div class="container mtb">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="row flex">
+          <div class="col-lg-12">
+            <form class="" action="" method="post">
+              <div class="input-group searchbox">
+                <input type="text" id="search" name="searchtext" class="form-control" placeholder="Search...">
+                <div class="input-group-btn">
+                  <button type="submit" name="submit" class="btn btn-default">
+                    <i class="glyphicon glyphicon-search"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <?php $showarchivedtexts = true; require_once('listtexts.php'); ?>
+      </div>
+    </div>
+  </div>
 
-<?php require_once('footer.php') ?>
+  <?php require_once('footer.php') ?>
--- public/index.php
+++ public/index.php
@@ -20,27 +20,54 @@ $_SESSION['actlangid'] = isset($row['prefActLangId']) ? $row['prefActLangId'] : 
 require_once('header.php');
 ?>
 
-<!-- *****************************************************************************************************************
+  <!-- *****************************************************************************************************************
 TABS
 ***************************************************************************************************************** -->
 
-<div class="container mtb">
-	<div class="row">
-		<div class="col-lg-12">
-			<!-- Tabs  -->
-			<ul class="nav nav-tabs" role="tablist">
-				<li role="presentation" class="active"><a href="#mytexts" aria-controls="mytexts" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-book"></span> My texts</a></li>
-				<li role="presentation" class="nav"><a href="#rss" aria-controls="rss" role="tab" data-toggle="tab"><span class="glyphicon glyphicon-fire"></span> RSS</a></li>
-			</ul>
+  <div class="container mtb">
+    <div class="row">
+      <div class="col-lg-12">
+        <!-- Tabs  -->
+        <ul class="nav nav-tabs" role="tablist">
+          <li role="presentation" class="active">
+            <a href="#mytexts" aria-controls="mytexts" role="tab" data-toggle="tab">
+              <span class="glyphicon glyphicon-book"></span> My texts</a>
+          </li>
+          <li role="presentation" class="nav">
+            <a href="#rss" aria-controls="rss" role="tab" data-toggle="tab">
+              <span class="glyphicon glyphicon-fire"></span> RSS</a>
+          </li>
+        </ul>
 
-			<!-- Tab panes: content inside tabs -->
-			<div class="tab-content">
-				<div role="tabpanel" class="tab-pane fade in active" id="mytexts"><?php $showarchivedtexts = false; require_once('listtexts.php') ?></div>
-				<div role="tabpanel" class="tab-pane fade" id="rss">chau</div>
-			</div>
-
-		</div>
-	</div>
-</div>
-
+        <!-- Tab panes: content inside tabs -->
+        <div class="tab-content">
+          <div role="tabpanel" class="tab-pane fade in active" id="mytexts">
+            <div class="row flex">
+              <div class="col-xs-12 col-md-9">
+                <form class="" action="" method="post">
+                  <div class="input-group searchbox">
+                    <input type="text" id="search" name="searchtext" class="form-control" placeholder="Search...">
+                    <div class="input-group-btn">
+                      <button type="submit" name="submit" class="btn btn-default">
+                        <i class="glyphicon glyphicon-search"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="col-xs-12 col-md-3 searchbox">
+                <button type="button" name="btn-addtext" class="btn btn-default" onclick="window.location='addtext.php'">
+                  <span class="glyphicon glyphicon-plus"></span> Add text</button>
+                <button type="file" name="btn-upload" class="btn btn-success">
+                  <span class="glyphicon glyphicon-upload"></span> Upload text</button>
+              </div>
+            </div>
+            <?php $showarchivedtexts = false; require_once('listtexts.php') ?>
+          </div>
+          <div role="tabpanel" class="tab-pane fade" id="rss">chau</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 <?php require_once('footer.php') ?>
