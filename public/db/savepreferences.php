dc1f07a87e7c5b0697980577f37e52c599027f2d
usemoslinux <pablo.castagnino@gmail.com>
2018-06-01 00:06:02 +0200

    Minor change in preferences

--- public/db/savepreferences.php
+++ public/db/savepreferences.php
@@ -10,16 +10,18 @@
     $actlangid = $_COOKIE['actlangid'];
 
     require_once('dbinit.php');
-
+    
     $result = mysqli_query($con, "REPLACE INTO preferences (prefUserId, prefFontFamily,
         prefFontSize, prefLineHeight, prefAlignment, prefMode, prefAssistedLearning, prefActLangId)
         VALUES ('$userid', '$fontfamily', '$fontsize', '$lineheight', '$alignment', '$mode', '$assistedlearning', '$actlangid')") or die(mysqli_error($con));
 
-    $expire = time()+81400;
-    setcookie('fontfamily', $fontfamily, $expire, "/", false, 0);
-    setcookie('fontsize', $fontsize, $expire, "/", false, 0);
-    setcookie('lineheight', $lineheight, $expire, "/", false, 0);
-    setcookie('alignment', $alignment, $expire, "/", false, 0);
-    setcookie('mode', $mode, $expire, "/", false, 0);
-    setcookie('assistedlearning', $assistedlearning, $expire, "/", false, 0);
+    if ($result) {
+      $expire = time()+81400;
+      setcookie('fontfamily', $fontfamily, $expire, "/", false, 0);
+      setcookie('fontsize', $fontsize, $expire, "/", false, 0);
+      setcookie('lineheight', $lineheight, $expire, "/", false, 0);
+      setcookie('alignment', $alignment, $expire, "/", false, 0);
+      setcookie('mode', $mode, $expire, "/", false, 0);
+      setcookie('assistedlearning', $assistedlearning, $expire, "/", false, 0);
+    }
  ?>
