dc16c84b4e146de8a3a02ee15d1afc04d3b9d150
usemoslinux <pablo.castagnino@gmail.com>
2018-06-03 15:43:14 +0200

    Fixed bug in statistics chart. The SQL to calculate the day was wrong.

--- public/db/getstats.php
+++ public/db/getstats.php
@@ -4,7 +4,7 @@
   
   // get how many words were created in each of the last 7 days
   for ($i=6; $i >= 0; $i--) { 
-    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE TIMESTAMPDIFF(DAY,wordCreated, now()) = $i") 
+    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordCreated < CURDATE() - INTERVAL $i-1 DAY AND wordCreated > CURDATE() - INTERVAL $i DAY") 
       or die(mysqli_error($con));
     $row = mysqli_fetch_array($result);
     $array['created'][] = $row[0];
@@ -12,7 +12,7 @@
 
   // get how many words' status were modified in each of the last 7 days
   for ($i=6; $i >= 0; $i--) { 
-    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus>0 AND TIMESTAMPDIFF(DAY,wordModified, now()) = $i") 
+    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus>0 AND wordModified < CURDATE() - INTERVAL $i-1 DAY AND wordModified > CURDATE() - INTERVAL $i DAY") 
       or die(mysqli_error($con));
     $row = mysqli_fetch_array($result);
     $array['modified'][] = $row[0];
@@ -20,7 +20,7 @@
 
   // get how many words were learned in each of the last 7 days
   for ($i=6; $i >= 0; $i--) { 
-    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus=0 AND TIMESTAMPDIFF(DAY,wordModified, now()) = $i") 
+    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus=0 AND wordModified < CURDATE() - INTERVAL $i-1 DAY AND wordModified > CURDATE() - INTERVAL $i DAY") 
       or die(mysqli_error($con));
     $row = mysqli_fetch_array($result);
     $array['learned'][] = $row[0];
@@ -28,7 +28,7 @@
 
   // get how many learned words were forgotten in each of the last 7 days
   for ($i=6; $i >= 0; $i--) { 
-    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus=2 AND wordModified>wordCreated AND TIMESTAMPDIFF(DAY,wordModified, now()) = $i") 
+    $result = mysqli_query($con, "SELECT COUNT(word) FROM words WHERE wordStatus=2 AND wordModified>wordCreated AND wordModified < CURDATE() - INTERVAL $i-1 DAY AND wordModified > CURDATE() - INTERVAL $i DAY") 
       or die(mysqli_error($con));
     $row = mysqli_fetch_array($result);
     $array['forgotten'][] = $row[0];
