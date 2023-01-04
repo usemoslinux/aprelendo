2d1dc277ebb7fc77ea4c367feed84f8f0c657514
Pablo Castagnino <pablo.castagnino@gmail.com>
2023-01-04 23:52:21 +0000

    fixed show/hide password buttons in register.php

--- public/js/password.js
+++ public/js/password.js
@@ -16,85 +16,84 @@
  * You should have received a copy of the GNU General Public License
  * along with aprelendo.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 $(document).ready(function() {
     /**
      * Checks password confirmation matches original password
      */
     function checkPasswordsAreEqual() {
-        let $password = $("#newpassword");
-        let $password_confirmation = $("#newpassword-confirmation");
-        let $text = $("#passwords-match-text");
+        const $password = $('#newpassword')
+        const $password_confirmation = $('#newpassword-confirmation')
+        const $text = $('#passwords-match-text')
 
-        if ($password.val() == "" || $password_confirmation.val() == "") {
-            $password_confirmation.css("border-bottom", "1px solid #ced4da");
-            $text.text("");
-        } else if ($password_confirmation.val() != $password.val()) {
-            $password_confirmation.css("border-bottom", "2px solid red");
-            $text.text("Passwords dont match");
+        if ($password.val() === '' || $password_confirmation.val() === '') {
+            $password_confirmation.css('border-bottom', '1px solid #ced4da')
+            $text.text('')
+        } else if ($password_confirmation.val() !== $password.val()) {
+            $password_confirmation.css('border-bottom', '2px solid red')
+            $text.text('Passwords dont match')
         } else {
-            $password_confirmation.css("border-bottom", "2px solid green");
-            $text.text("Passwords match");
+            $password_confirmation.css('border-bottom', '2px solid green')
+            $text.text('Passwords match')
         }
     } // end checkPasswordsAreEqual
 
     /**
      * Checks password strength and changes progress bar accordingly
      */
-    $("#newpassword").on("input", function() {
-        const number = /([0-9])/;
-        const letters = /([a-zA-Z])/;
-        const special_chars = /([~`!@#$%^&*()\-_+={};:\[\]\?\.\/,])/;
+    $('#newpassword').on('input', function() {
+        const number = /([0-9])/
+        const letters = /([a-zA-Z])/
+        const special_chars = /([~`!@#$%^&*()\-_+={};:[\]?./,])/
 
-        let $password = $(this);
-        let $password_confirmation = $("#newpassword-confirmation");
-        let $text = $("#password-strength-text");
+        const $password = $(this)
+        const $password_confirmation = $('#newpassword-confirmation')
+        const $text = $('#password-strength-text')
 
         if ($password.val().length < 8) {
-            $password.css("border-bottom", "2px solid red");
-            $text.text("Weak (should be at least 8 characters long)");
+            $password.css('border-bottom', '2px solid red')
+            $text.text('Weak (should be at least 8 characters long)')
         } else if (
             $password.val().match(number) &&
             $password.val().match(letters) &&
             $password.val().match(special_chars)
         ) {
-            $password.css("border-bottom", "2px solid green");
-            $text.text("Strong");
+            $password.css('border-bottom', '2px solid green')
+            $text.text('Strong')
         } else {
-            $password.css("border-bottom", "2px solid yellow");
+            $password.css('border-bottom', '2px solid yellow')
             $text.text(
-                "Medium (should include letters, numbers and special characters)"
-            );
+                'Medium (should include letters, numbers and special characters)'
+            )
         }
 
-        if ($password_confirmation.val() != "") {
-            checkPasswordsAreEqual();
+        if ($password_confirmation.val() !== '') {
+            checkPasswordsAreEqual()
         }
-    }); // end #newpassword.on.input
+    }) // end #newpassword.on.input
 
     /**
      * Triggered when user is writing password confirmation
      */
-    $("#newpassword-confirmation").on("input", function() {
-        checkPasswordsAreEqual();
-    }); // end #newpassword-confirmation.on.input
+    $('#newpassword-confirmation').on('input', function() {
+        checkPasswordsAreEqual()
+    }) // end #newpassword-confirmation.on.input
 
     /**
      * Shows/hides password
      */
-    $(".show-hide-password-btn").on("click", function(e) {
-        e.preventDefault();
+    $('.show-hide-password-btn').on('click', function(e) {
+        e.preventDefault()
         let $password_input = $(this)
             .parent()
-            .siblings("input");
-        let $password_i = $(this).find("i");
+            .find('input')
+        let $password_span = $(this).find('span')
 
-        if ($password_input.attr("type") == "text") {
-            $password_input.attr("type", "password");
-            $password_i.addClass("fa-eye-slash").removeClass("fa-eye");
-        } else if ($password_input.attr("type") == "password") {
-            $password_input.attr("type", "text");
-            $password_i.removeClass("fa-eye-slash").addClass("fa-eye");
+        if ($password_input.attr('type') === 'text') {
+            $password_input.attr('type', 'password')
+            $password_span.addClass('fa-eye-slash').removeClass('fa-eye')
+        } else if ($password_input.attr('type') === 'password') {
+            $password_input.attr('type', 'text')
+            $password_span.removeClass('fa-eye-slash').addClass('fa-eye')
         }
-    }); // end .show-hide-password-btn.on.click
-});
+    }) // end .show-hide-password-btn.on.click
+})
