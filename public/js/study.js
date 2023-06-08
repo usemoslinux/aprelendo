771edb180dd420fa73ec69772d23a7d97c091ed9
Pablo Castagnino <pablo.castagnino@gmail.com>
2023-06-08 22:56:11 +0000

    minor fix to last commit

--- public/js/study.js
+++ public/js/study.js
@@ -123,7 +123,7 @@ $(document).ready(function() {
                 let examples_count = 0;
                 const lang_iso = $("#card").data("lang");
                 let sentence_regex = new RegExp(
-                    '([^\\n.?!]|[\\d][.][\\d]|\\p{L}[.])*(?<![\\p{L}])' + word + '(?![\\p{L}])([^\\n.?!]|[.][\\d]|[.]\\p{L})*[\\n.?!]',
+                    '([^\\n.?!]|[\\d][.][\\d]|[A-Z][.](?:[A-Z][.])+)*(?<![\\p{L}])' + word + '(?![\\p{L}])([^\\n.?!]|[.][\\d]|[.](?:[A-Z][.])+)*[\\n.?!]',
                     'gmiu'
                 );
 
