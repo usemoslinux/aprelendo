a417bcd5737c1afe91d0a9b5eccb979bcf6553e9
Pablo Castagnino <pablo.castagnino@gmail.com>
2024-02-13 19:22:45 +0000

    minor fix to last commit

--- public/showdicmodal.php
+++ public/showdicmodal.php
@@ -21,7 +21,7 @@
 ?>
 
 <div id="dic-modal" class="modal fade" data-keyboard="true" role="dialog" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content mb-xs-3">
             <div class="modal-header p-2">
