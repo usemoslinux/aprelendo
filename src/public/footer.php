5b1b593da1fc175d5ce6d8d0cfe81af3b59f199f
Pablo Castagnino <pablo.castagnino@gmail.com>
2024-11-17 23:06:41 +0000

    added github & blog to page footer, and updated twitter logo

--- src/public/footer.php
+++ src/public/footer.php
@@ -49,6 +49,12 @@
                 </ul>
                 <ul class="list-inline">
                     <li class="list-inline-item">
+                        <a href="https://blog.aprelendo.com" aria-label="Aprelendo Blog"
+                            target="_blank" rel="noopener noreferrer">
+                            <span class="bi bi-wordpress"></span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
                         <a href="https://www.facebook.com/aprelendo.fb" aria-label="Aprelendo's Facebook Page"
                             target="_blank" rel="noopener noreferrer">
                             <span class="bi bi-facebook"></span>
@@ -63,13 +69,19 @@
                     <li class="list-inline-item">
                         <a href="https://twitter.com/aprelendo" aria-label="Aprelendo's Twitter Page" target="_blank"
                             rel="noopener noreferrer">
-                            <span class="bi bi-twitter"></span>
+                            <span class="bi bi-twitter-x"></span>
                         </a>
                     </li>
                     <li class="list-inline-item">
                         <a href="https://www.youtube.com/channel/UCc5gaiAsGU4sC09mvVjibCA"
                             aria-label="Aprelendo's YouTube channel" target="_blank" rel="noopener noreferrer">
                             <span class="bi bi-youtube"></span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="https://github.com/usemoslinux/aprelendo" aria-label="Github"
+                            target="_blank" rel="noopener noreferrer">
+                            <span class="bi bi-github"></span>
                         </a>
                     </li>
                 </ul>
