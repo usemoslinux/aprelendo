c1bfe73fb9960f5b85cd0ffd54c68983ed296587
Pablo Castagnino <pablo.castagnino@gmail.com>
2026-01-01 00:56:16 +0000

    refactor: improved Wikipedia article parsing and text normalization

--- src/public/js/addtext.js
+++ src/public/js/addtext.js
@@ -17,7 +17,7 @@
  * along with aprelendo.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Check for an external api call. If detected, try to fetch text using Mozilla's readability parser
     if ($("#external_call").length) {
         fetch_text($("#url").val());
@@ -27,16 +27,16 @@ $(document).ready(function() {
      * Adds text to database
      * This is triggered when user presses the "Save" button & submits the form
      */
-    $("#form-addtext").on("submit", function(e) {
+    $("#form-addtext").on("submit", function (e) {
         e.preventDefault();
 
         const form_data = new FormData(document.getElementById("form-addtext"));
         const get_params = new URLSearchParams(window.location.search);
         const id = get_params.get("id");
         const url = !id ? "ajax/addtext.php" : "ajax/edittext.php";
-        
+
         if (id) {
-            form_data.append("id", id);    
+            form_data.append("id", id);
         }
 
         $.ajax({
@@ -47,7 +47,7 @@ $(document).ready(function() {
             contentType: false,
             processData: false
         })
-            .done(function(data) {
+            .done(function (data) {
                 if (typeof data != "undefined") {
                     showMessage(data.error_msg, "alert-danger");
                 } else if (form_data.get("shared-text") == "on") {
@@ -56,7 +56,7 @@ $(document).ready(function() {
                     window.location.replace("/texts");
                 }
             })
-            .fail(function(xhr, ajaxOptions, thrownError) {
+            .fail(function (xhr, ajaxOptions, thrownError) {
                 showMessage(
                     "Oops! There was an unexpected error processing this text.",
                     "alert-danger"
@@ -67,7 +67,7 @@ $(document).ready(function() {
     /**
      * Checks how many characters are left for user input
      */
-    $("#text").on("input", function() {
+    $("#text").on("input", function () {
         updateCharsLeft();
     }); // end #text.on.input
 
@@ -75,10 +75,10 @@ $(document).ready(function() {
      * Inserts text from file in textarea
      * This is triggered when the user clicks the "upload" text button
      */
-    $("#upload-text").on("change", function() {
+    $("#upload-text").on("change", function () {
         const file = $(this)[0].files[0];
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             const text = e.target.result;
             $("#text").val(text);
             updateCharsLeft();
@@ -90,7 +90,7 @@ $(document).ready(function() {
     /**
      * Process text pasted inside textarea
      */
-    $("#text").on("paste", function(e) {
+    $("#text").on("paste", function (e) {
         updateCharsLeft();
     });
 
@@ -98,7 +98,7 @@ $(document).ready(function() {
      * Fetch text from URL
      * This is triggered when user clicks the Fetch button
      */
-    $("#btn-fetch").on("click", function(e) {
+    $("#btn-fetch").on("click", function (e) {
         fetch_text($("#url").val());
     });
 
@@ -106,7 +106,7 @@ $(document).ready(function() {
      * Automatically fetch text from URL
      * This is triggered when user pastes a URL 
      */
-    $("#url").on("paste", function(e) {
+    $("#url").on("paste", function (e) {
         const text = $("#text").val();
         // only auto-fetch text if textarea is empty
         if (!text || /^\s*$/.test(text)) {
@@ -118,16 +118,16 @@ $(document).ready(function() {
     /**
      * Checks if audio URL is a Google Drive link
      */
-    $('#audio-url').on('input', function() {
+    $('#audio-url').on('input', function () {
         const audio_url = $(this).val();
         const help_text = $('#audio-url-helptext');
-    
+
         if (audio_url.includes('drive.google.com')) {
             help_text.html('<i class="bi bi-cloud-fill"></i> Remember to <a href="https://support.google.com/drive/answer/2494822?hl=en&co=GENIE.Platform%3DDesktop#zippy=%2Cshare-a-file-publicly" target="_blank" rel="noopener noreferrer" class="alert-link">share this file publicly</a>, allowing access to anyone with the link.');
         } else {
             help_text.text('Accepts URLs from Google Drive or any standard audio source.');
         }
-    });    
+    });
 
     /**
      * Resets control values, i.e. empty form
@@ -174,8 +174,7 @@ $(document).ready(function() {
      * Checks that the URL passed as a parameter is valid
      * @param {string} url
      */
-    function isValidUrl(url)
-    {
+    function isValidUrl(url) {
         try {
             new URL(url);
             return true;
@@ -183,7 +182,7 @@ $(document).ready(function() {
             return false;
         }
     }
-    
+
     function fetch_text(url) {
         if (isWikiArticle(url)) {
             const urlObj = new URL(url);
@@ -211,13 +210,13 @@ $(document).ready(function() {
             $("#btn-fetch-img")
                 .removeClass()
                 .addClass("spinner-border spinner-border-sm text-warning");
-                
+
             $.ajax({
                 type: "GET",
                 url: "ajax/fetchurl.php",
                 data: { url: url }
             })
-                .done(function(data) {
+                .done(function (data) {
                     if (data.error_msg != null) {
                         showMessage(
                             "It was not possible to extract the text from the URL you provided. " +
@@ -232,7 +231,7 @@ $(document).ready(function() {
                                 "matches your selected language before proceeding. This helps keep your learning " +
                                 "experience focused and organized!", "alert-warning");
                         }
-                        
+
                         const doc = document.implementation.createHTMLDocument(
                             "New Document"
                         );
@@ -285,13 +284,13 @@ $(document).ready(function() {
                         );
                     }
                 })
-                .fail(function(xhr, ajaxOptions, thrownError) {
+                .fail(function (xhr, ajaxOptions, thrownError) {
                     showMessage(
                         "There was an unexpected error trying to fetch this text.",
                         "alert-danger"
                     );
                 })
-                .always(function() {
+                .always(function () {
                     $("#btn-fetch-img")
                         .removeClass()
                         .addClass("bi bi-arrow-down-right-square text-warning");
@@ -308,8 +307,8 @@ $(document).ready(function() {
     function isWikiArticle(textURI) {
         const text_lang = $('#text').data('text-lang');
         const standard_uri = `https://${text_lang}.wikipedia.org/wiki/`;
-        const mobile_uri   = `https://${text_lang}.m.wikipedia.org/wiki/`;
-      
+        const mobile_uri = `https://${text_lang}.m.wikipedia.org/wiki/`;
+
         return textURI.startsWith(standard_uri) || textURI.startsWith(mobile_uri);
     }
 
@@ -326,32 +325,36 @@ $(document).ready(function() {
             explaintext: true,
             origin: '*'  // This is for CORS
         };
-    
+
         $.ajax({
             url: url,
             method: 'GET',
             data: params
         })
-        .done(function(data) {
-            const page = data.query.pages[Object.keys(data.query.pages)[0]];
-            let modifiedpage = removeNumbersInBrackets(page.extract);
-            modifiedpage = addLineBreaksAfterDots(modifiedpage);
-            modifiedpage = normalizeParagraphBreaks(modifiedpage)
+            .done(function (data) {
+                const page = data.query.pages[Object.keys(data.query.pages)[0]];
+                let raw_text = page.extract;
 
-            const readable_title = decodeURIComponent(title.replace(/_/g, ' '));    
+                let clean_text = stripTrailingSections(raw_text, text_lang);
+                clean_text = removeNumbersInBrackets(clean_text);
+                // clean_text = addLineBreaksAfterDots(clean_text);
+                clean_text = normalizeParagraphBreaks(clean_text);
+                clean_text = cleanWikipediaTitles(clean_text);
 
-            $("#title").val(readable_title);
-            $("#text").val(modifiedpage);
-            updateCharsLeft();
-        })
-        .fail(function(xhr, ajaxOptions, thrownError) {
-            showMessage(
-                "There was an unexpected error trying to fetch this Wikipedia article.",
-                "alert-danger"
-            );
-        });
+                const readable_title = decodeURIComponent(title.replace(/_/g, ' '));
+
+                $("#title").val(readable_title);
+                $("#text").val(clean_text);
+                updateCharsLeft();
+            })
+            .fail(function (xhr, ajaxOptions, thrownError) {
+                showMessage(
+                    "There was an unexpected error trying to fetch this Wikipedia article.",
+                    "alert-danger"
+                );
+            });
     }
-    
+
     /**
      * Removes all numbers in brackets commonly used in Wikipedia articles to indicate references
      * @param {string} text 
@@ -362,21 +365,82 @@ $(document).ready(function() {
     }
 
     /**
-     * Replace a dot followed by a word character with a dot plus two line breaks, 
-     * but only if it's not followed by another dot (common in acronyms)
-     * @param {string} text 
-     * @returns string
+     * Cleans Wikipedia-style headings by removing all '=' characters
+     * and any surrounding whitespace.
+     * @param {string} text - Raw text that may contain Wikipedia heading markers
+     * @returns {string} Cleaned text without '=' markers or extra whitespace
      */
-    function addLineBreaksAfterDots(text) {
-        return text.replace(/\.(?=\p{L})(?!\p{L}\.)/gu, '.\n\n');
+    function cleanWikipediaTitles(text) {
+        return text.replace(/[ \t]*=+[ \t]*/g, '');
     }
 
     /**
-     * Separates all paragraphs with two line breaks
+     * Separates all paragraphs with two line breaks.
+     * Recognizes paragraphs separated by multiple newlines, even if those 
+     * lines contain spaces or tabs.
      * @param {string} text 
      * @returns string
      */
     function normalizeParagraphBreaks(text) {
-        return text.replace(/(\r\n|\r|\n)+/g, "\n\n");
+        // This regex matches:
+        // 1. A newline (\r\n|\r|\n)
+        // 2. Followed by any combination of newlines AND horizontal whitespace (\s matches both, 
+        // but we specifically target vertical/horizontal mix)
+        // The [ \t\r\n]+ ensures that if there is a "blank" line with just spaces, it is treated as part of the break.
+        return text.replace(/(\r\n|\r|\n)([ \t\r\n]*)/g, "\n\n");
+    }
+
+    /**
+     * Truncates the text at the first occurrence of a "junk" section header
+     * based on the current language.
+     */
+    function stripTrailingSections(text, lang) {
+        const sectionTerms = {
+            'ar': ['انظر أيضا', 'مراجع', 'وصلات خارجية'],
+            'bg': ['Вижте също', 'Източници', 'Външни препратки'],
+            'ca': ['Vegeu també', 'Referències', 'Enllaços externs'],
+            'cs': ['Viz též', 'Reference', 'Externí odkazy'],
+            'da': ['Se også', 'Referencer', 'Eksterne henvisninger'],
+            'de': ['Siehe auch', 'Einzelnachweise', 'Weblinks', 'Literatur'],
+            'el': ['Δείτε επίσης', 'Παραπομπές', 'Εξωτερικοί σύνδεσμοι'],
+            'en': ['See also', 'References', 'Further reading', 'External links'],
+            'es': ['Véase también', 'Referencias', 'Bibliografía', 'Enlaces externos'],
+            'fr': ['Voir aussi', 'Notes et références', 'Liens externes'],
+            'he': ['ראו גם', 'הערות שוליים', 'קישורים חיצוניים'],
+            'hi': ['इन्हें भी देखें', 'सन्दर्भ', 'बाहरी कड़ियाँ'],
+            'hr': ['Vidi još', 'Izvori', 'Vanjske poveznice'],
+            'hu': ['Lásd még', 'Jegyzetek', 'Források', 'További információk'],
+            'it': ['Voci correlate', 'Note', 'Bibliografia', 'Collegamenti esterni'],
+            'ja': ['関連項目', '脚注', '参考文献', '外部リンク'],
+            'ko': ['같이 보기', '각주', '참고 문헌', '외부 링크'],
+            'nl': ['Zie ook', 'Bronnen', 'Externe links'],
+            'no': ['Se også', 'Referanser', 'Eksterne lenker'],
+            'pl': ['Zobacz też', 'Przypisy', 'Bibliografia', 'Linki zewnętrzne'],
+            'pt': ['Ver também', 'Referências', 'Ligações externas'],
+            'ro': ['Vezi și', 'Note', 'Referințe', 'Legături externe'],
+            'ru': ['См. также', 'Примечания', 'Литература', 'Ссылки'],
+            'sk': ['Pozri aj', 'Referencie', 'Externé odkazy'],
+            'sl': ['Glej tudi', 'Sklici', 'Viri', 'Zunanje povezave'],
+            'sv': ['Se även', 'Referenser', 'Externa länkar'],
+            'tr': ['Ayrıca bakınız', 'Kaynakça', 'Dış bağlantılar'],
+            'vi': ['Xem thêm', 'Tham khảo', 'Liên kết ngoài'],
+            'zh': ['参见', '参考文献', '外部链接']
+        };
+
+        const terms = sectionTerms[lang] || sectionTerms['en'];
+
+        // Create a regex that looks for:
+        // 1. Optional newlines
+        // 2. Exactly two or more "=" signs
+        // 3. Optional whitespace
+        // 4. Any of our language-specific terms
+        // 5. Optional whitespace and trailing "=" signs
+        const pattern = new RegExp(`(\\n+|^)\\s*={2,}\\s*(${terms.join('|')})\\s*={2,}`, 'i');
+
+        const match = text.search(pattern);
+        if (match !== -1) {
+            return text.substring(0, match);
+        }
+        return text;
     }
 });
