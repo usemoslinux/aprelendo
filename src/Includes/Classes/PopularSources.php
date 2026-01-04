0cc9a484218e19ad18be1f8b045f4aae9a47b632
Pablo Castagnino <pablo.castagnino@gmail.com>
2026-01-04 23:03:58 +0000

    refactor: popular sources invalid sources

--- src/Includes/Classes/PopularSources.php
+++ src/Includes/Classes/PopularSources.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright (C) 2019 Pablo Castagnino
  *
@@ -45,27 +46,20 @@ class PopularSources extends DBEntity
      */
     public function add(string $lg_iso, string $domain): void
     {
-        $invalid_sources = [
-            'feedproxy.google.com',
-            'www.youtube.com',
-            'm.youtube.com',
-            'youtu.be'
-        ];
-
-        if (!isset($lg_iso) || empty($lg_iso) || !isset($domain) || empty($domain)) {
+        if (empty(trim($lg_iso)) || empty(trim($domain))) {
             return;
         }
 
-        $domain = mb_strtolower($domain);
-        // if text belongs to an invalid source or is an ebook, avoid adding it to popular_sources table
-        if (in_array($domain, $invalid_sources) || pathinfo($domain, PATHINFO_EXTENSION) === '.epub') {
+        $domain = $this->normalizeDomain($domain);
+
+        if ($this->isInvalid($domain)) {
             return;
         }
 
         $sql = "INSERT INTO `{$this->table}` (`lang_iso`, `times_used`, `domain`)
                 VALUES (?, 1, ?) ON DUPLICATE KEY UPDATE `times_used` = `times_used` + 1";
         $this->sqlExecute($sql, [$lg_iso, $domain]);
-    } // end add()
+    }
 
     /**
      * Updates existing domain in database
@@ -76,16 +70,20 @@ class PopularSources extends DBEntity
      */
     public function update(string $lg_iso, string $domain): void
     {
-        if (!isset($lg_iso) || empty($lg_iso) || !isset($domain) || empty($domain)) {
+        if (empty(trim($lg_iso)) || empty(trim($domain))) {
             return;
         }
 
-        $sql = "DELETE FROM `{$this->table}` WHERE `lang_iso`=? AND `domain`=? AND `times_used` = 1";
+        $domain = $this->normalizeDomain($domain);
+
+        // Delete if it's currently at 1 use (or safety check for <= 1)
+        $sql = "DELETE FROM `{$this->table}` WHERE `lang_iso`=? AND `domain`=? AND `times_used` <= 1";
         $this->sqlExecute($sql, [$lg_iso, $domain]);
-                
+
+        // Decrement the rest
         $sql = "UPDATE `{$this->table}` SET `times_used`=`times_used` - 1 WHERE `lang_iso`=? AND `domain`=?";
         $this->sqlExecute($sql, [$lg_iso, $domain]);
-    } // end update()
+    }
 
     /**
      * Get all rows for a given language
@@ -100,7 +98,119 @@ class PopularSources extends DBEntity
         }
 
         $sql = "SELECT * FROM `{$this->table}` WHERE `lang_iso`=? ORDER BY `times_used` DESC LIMIT 50";
-        
+
         return $this->sqlFetchAll($sql, [$lg_iso]);
     } // end getAllByLang()
+
+    /**
+     * Checks if a domain or extension is on the blacklist.
+     * 
+     * @param string $domain
+     * @return bool
+     */
+    private function isInvalid(string $domain): bool
+    {
+        $invalid_sources = [
+            // RSS / Search
+            'feedproxy.google.com',
+            'google.com',
+            'bing.com',
+            'yahoo.com',
+            'duckduckgo.com',
+            'baidu.com',
+            'yandex.ru',
+            // Video / Media
+            'youtube.com',
+            'youtu.be',
+            'vimeo.com',
+            'dailymotion.com',
+            'twitch.tv',
+            'soundcloud.com',
+            // Social / Aggregators
+            'facebook.com',
+            'm.facebook.com',
+            't.me',
+            'instagram.com',
+            'reddit.com',
+            'pinterest.com',
+            'linkedin.com',
+            'tiktok.com',
+            'twitter.com',
+            'x.com',
+            // URL Shorteners
+            'bit.ly',
+            't.co',
+            'goo.gl',
+            'tinyurl.com',
+            'ow.ly',
+            'is.gd',
+            'buff.ly',
+            'lnkd.in',
+            'bit.do',
+            // CDNs & Infrastructure (Usually non-content sources)
+            'cloudfront.net',
+            'akamaihd.net',
+            'fastly.net',
+            'cloudinary.com',
+            'wp.com',
+            's3.amazonaws.com',
+            // Email & Internal
+            'gmail.com',
+            'outlook.com',
+            'hotmail.com',
+            'localhost',
+            '127.0.0.1'
+        ];
+
+        // document formats and archives
+        $invalid_extensions = [
+            'epub',
+            'pdf',
+            'zip',
+            'mp3',
+            'mp4',
+            'onion',
+            'docx',
+            'xlsx',
+            'pptx',
+            'tar',
+            'gz',
+            '7z',
+            'rar',
+            'iso'
+        ];
+
+        $extension = pathinfo($domain, PATHINFO_EXTENSION);
+
+        // check direct match in blacklist
+        if (in_array($domain, $invalid_sources)) {
+            return true;
+        }
+
+        // check forbidden extensions
+        if (in_array($extension, $invalid_extensions)) {
+            return true;
+        }
+
+        // catch-all for subdomains (e.g., "anything.facebook.com")
+        foreach ($invalid_sources as $bad_domain) {
+            if (str_ends_with($domain, '.' . $bad_domain)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Normalizes the domain for consistency across the database.
+     * 
+     * @param string $domin
+     * @return string
+     */
+    private function normalizeDomain(string $domain): string
+    {
+        $domain = mb_strtolower(trim($domain));
+        return preg_replace('/^www\./', '', $domain);
+    }
 }
